0c6004f5a46f2aba555bfafb2bf3cc48efed974e
apisitkitti <apisitkitti@gmail.com>
2026-03-03 22:25:31 +0700

    feat: add card component

---
+++ src/data/profileData.ts
@@ -0,0 +1,34 @@
+export const PROFILE_INFORMATION = [
+  {
+    title: "Phiilip Pocket",
+    img: "/profile.jpg",
+    time: "2026",
+    categorys: ["UX/UI", "Mobile App"],
+    about:
+      "Redesigned the FAME website a platform used by financial advisors (FAs) at Phillip to modernize its interface and ensure it supports upcoming features and client management tasks.",
+  },
+  {
+    title: "Phiilip FAME",
+    img: "/profile.jpg",
+    time: "2026",
+    categorys: ["UX/UI", "Admin Website"],
+    about:
+      "Redesigned the FAME website a platform used by financial advisors (FAs) at Phillip to modernize its interface and ensure it supports upcoming features and client management tasks.",
+  },
+  {
+    title: "Design System",
+    img: "/profile.jpg",
+    time: "2025 - Present ",
+    categorys: ["Design System"],
+    about:
+      "Redesigned the FAME website a platform used by financial advisors (FAs) at Phillip to modernize its interface and ensure it supports upcoming features and client management tasks.",
+  },
+  {
+    title: "AVATR",
+    img: "/profile.jpg",
+    time: "2026",
+    categorys: ["Freelance", "Website"],
+    about:
+      "Redesigned the FAME website a platform used by financial advisors (FAs) at Phillip to modernize its interface and ensure it supports upcoming features and client management tasks.",
+  },
+];
